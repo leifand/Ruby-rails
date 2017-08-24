383ec1668fae4e6b7919cde6dc84267cba360133
Leif Anderson <leif.v.anderson@gmail.com>
2017-08-24 04:35:41 -0500

    network - in progress

---
+++ network/app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  # restful routes not fully implemented, but add checks anyway
+  before_filter(only: [:edit, :update, :destroy]) { require_correct_user }
+
+  def index
+    @users = User.where.not(:id => current_user.id)
+    #@shoes = Shoe.where.not(:user_id => current_user.id)
+    # Idea.joins(:likes).order('count(likes.id) DESC')
+    # Idea.joins(:likes)
+    # Idea.joins(left join likes on likes.idea_id = secrets.id').group(:id).order('count likes.id desc')
+    # Idea.all.sort{|a, b| b.likes.count <=> a.likes.count}
+  end
+
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+          flash[:notice] = ["Registered successfully"]
+          session[:user_id] = @user.id
+          return redirect_to dashboard_path
+      end
+      errors = @user.errors.full_messages
+      flash[:errors] = errors
+      return redirect_to :back
+    end
+
+    private def user_params
+      params.require(:user).permit(:name, :email, :description, :password, :password_confirmation)
+    end
+
+    private def require_correct_user
+      unless params[:id].to_i == session[:user_id]
+        return redirect_to "/users/#{session[:user_id]}"
+      end
+    end
+end
---
+++ network/config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # Application Routes
+  root 'application#index'
+  # User Routes
+  resources :users
+  get 'dashboard' => 'users#dashboard'
+  # Session Routes
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  # Event Routes
+  #resources :shoes
+  #post 'ideas/:id/user' => 'ideas#create'
+  #delete 'ideas/:id/user' => 'ideas#destroy'
+  # Like Routes
+  #get 'dashboard/:id' => 'shoes#dashboard'
+  #get 'sales/:id' => 'sales#create'
+end
